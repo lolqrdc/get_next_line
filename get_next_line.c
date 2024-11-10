77b1d25a4e62b8a542c941b2f5aaee5fd27ba629
lolqrdc <lola-rdc@outlook.fr>
2024-11-10 21:22:22 +0100

    plan upgrade for gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lolq <lolq@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/10 12:43:08 by loribeir          #+#    #+#             */
-/*   Updated: 2024/11/10 21:19:03 by lolq             ###   ########.fr       */
+/*   Updated: 2024/11/10 21:21:35 by lolq             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ char	*get_next_line(int fd)
 {
 /*
 1. static variable to stock.
+
 2. check if fd & buffer size are valid
+
 3. while stock do not have '/n' 
 	-> read buffer size from fd
 	-> add characters to stock 
@@ -28,10 +30,15 @@ char	*get_next_line(int fd)
 		> remove extracted characters from stock
 	-> if not found 
 		> extract all the stock as a new string
+
 5. allocate memory for the extracted string
+
 6. copy the extracted string to the new string
+
 7. clean the stock by removing the extracted string
+
 8. if its the end of fd, free the stock
+
 9. return a pointer to the new string allocated
 */
 }
